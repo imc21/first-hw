a053e5c38ed74f14e3ba0191be808c46888a6d63
Caleb Im <imcy@farmingdale.edu>
2021-09-13 12:44:18 -0400

    initial commit -project created and done

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,20 @@
+
+public class NewClass {
+    
+    public static void main(String[] args) {
+        
+        
+        System.out.println("List of even numbers from 0-100");
+        
+        for(int i = 0; i <= 100; i++)
+        {
+            if (i % 2 ==0)
+            {
+                System.out.println(i);
+            }
+        
+        
+   
+        }
+    }
+}
